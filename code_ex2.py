cb41db844c16b1ce15c77188933e66c6951b75b4
Roy Schossberge <royschossberge@cs.huji.ac.il>
2021-12-15 17:20:05 +0200

    After problem 4 kmean

--- code_ex2.py
+++ code_ex2.py
@@ -1,30 +1,40 @@
+import itertools
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+
 
 MIN_RANGE = 0
 MAX_RANGE = 1
 DIM = 1
 
 
-def get_uniform_data(num_of_points, range_x, range_y):
-    data_x = np.random.uniform(range_x[MIN_RANGE], range_x[MAX_RANGE], (DIM, num_of_points))
-    data_y = np.random.uniform(range_y[MIN_RANGE], range_y[MAX_RANGE], (DIM, num_of_points))
-    return data_x, data_y
+def get_uniform_data(num_of_points, plot):
+    data_x = np.random.uniform(-10, 2, (DIM, num_of_points))
+    data_y = np.random.uniform(18, 45, (DIM, num_of_points))
+    if plot:
+        plt.scatter(data_x, data_y)
+        plt.show()
+    data = merge_x_y(data_x.reshape(1, 500).tolist()[0], data_y.reshape(1, 500).tolist()[0])
+    return data
 
 
-def get_gaussian_data(centers, std, num_of_points):
+def get_gaussian_data(centers, std, num_of_points, plot):
     data = list()
     for center in centers:
         data_x = np.random.normal(center, std * center, num_of_points)
         data_y = np.random.normal(-center, std * center, num_of_points)
-        data.append((data_x, data_y))
+        temp_data = merge_x_y(data_x.tolist(), data_y.tolist())
+        if plot:
+            plt.scatter(data_x, data_y)
+            plt.show()
+        data.append(temp_data)
+    data = np.array(data)
     return data
-    #     plt.scatter(data_x, data_y)
-    # plt.show()
 
 
-def get_moon_b(range_x_1, range_x_2):
+def get_moon_b(range_x_1, range_x_2, plot):
     data_x_1 = np.random.uniform(range_x_1[0], range_x_1[1], 250)
     data_x_2 = np.random.uniform(range_x_2[0], range_x_2[1], 250)
 
@@ -35,15 +45,20 @@ def get_moon_b(range_x_1, range_x_2):
 
     data_x_1 += np.random.uniform(-0.1, 0.1, 250)
     data_x_2 += np.random.uniform(-0.1, 0.1, 250)
+    if plot:
+        plt.scatter(data_x_1, data_y_1)
+        plt.scatter(data_x_2, data_y_2)
+        plt.ylim((-1.5, 2.2))
+        plt.xlim((-1.5, 2.2))
+        plt.show()
+    else:
+        data_y = np.concatenate([data_y_1, data_y_2])
+        data_x = np.concatenate([data_x_1, data_x_2])
+        data = merge_x_y(data_x.tolist(), data_y.tolist())
+        return data
 
-    plt.scatter(data_x_1, data_y_1)
-    plt.scatter(data_x_2, data_y_2)
-    plt.ylim((-1.5, 2.2))
-    plt.xlim((-1.5, 2.2))
-    plt.show()
 
-
-def get_moon_c(range_x_1, range_x_2):
+def get_moon_c(range_x_1, range_x_2, plot):
     data_x_1 = np.random.uniform(range_x_1[0], range_x_1[1], 250)
     data_x_2 = np.random.uniform(range_x_2[0], range_x_2[1], 250)
 
@@ -52,14 +67,20 @@ def get_moon_c(range_x_1, range_x_2):
     data_y_2 = 1 - (data_x_2 - 1) ** 2
     data_y_2 = -1 * np.sqrt(data_y_2) + 0.5 + np.random.uniform(0.1, 0.4, 250)
 
-    plt.scatter(data_x_1, data_y_1)
-    plt.scatter(data_x_2, data_y_2)
-    plt.ylim((-1.5, 2.5))
-    plt.xlim((-1.5, 2.5))
-    plt.show()
+    if plot:
+        plt.scatter(data_x_1, data_y_1)
+        plt.scatter(data_x_2, data_y_2)
+        plt.ylim((-1.5, 2.2))
+        plt.xlim((-1.5, 2.2))
+        plt.show()
+    else:
+        data_y = np.concatenate([data_y_1, data_y_2])
+        data_x = np.concatenate([data_x_1, data_x_2])
+        data = merge_x_y(data_x.tolist(), data_y.tolist())
+        return data
 
 
-def get_letters_data():
+def get_letters_data(plot):
     # c plot:
     data_x_1 = np.random.uniform(-2, 0, 250)
     data_y_1 = 1 - (data_x_1 + 1) ** 2
@@ -92,34 +113,63 @@ def get_letters_data():
     data_x_2 = np.concatenate([data_x_2, data_x_3])
     data_y_2 = np.concatenate([data_y_2, data_y_3])
     # plot
-    plt.scatter(data_x_2, data_y_2)
-    plt.ylim((-3, 3))
-    plt.xlim((-3, 3))
-    plt.show()
+    if plot:
+        plt.scatter(data_x_2, data_y_2)
+        plt.ylim((-3, 3))
+        plt.xlim((-3, 3))
+        plt.show()
+    else:
+        data_y = np.concatenate([data_y_1, data_y_2])
+        data_x = np.concatenate([data_x_1, data_x_2])
+        data = merge_x_y(data_x.tolist(), data_y.tolist())
+        return data
 
 
-def get_horizontal_clamps(centers, std_x, std_y, num_of_points):
+def get_horizontal_clamps(centers, std_x, std_y, num_of_points, plot):
     # (0,0) , (5,0), (0,2), (5,2)
-    data = list()
+    data_x = np.array([])
+    data_y = np.array([])
     for center in centers:
-        data_x = np.random.normal(center[0], std_x, num_of_points)
-        data_y = np.random.normal(center[1], std_y, num_of_points)
-        data.append((data_x, data_y))
+        data_x = np.concatenate([data_x, np.random.normal(center[0], std_x, num_of_points)])
+        data_y = np.concatenate([data_y, np.random.normal(center[1], std_y, num_of_points)])
+    data = merge_x_y(data_x.tolist(), data_y.tolist())
+    if plot:
         plt.scatter(data_x, data_y)
         plt.xlim((-3, 9))
         plt.ylim((-3, 7))
-    plt.show()
+        plt.show()
+    else:
+        return data
 
 
-def plot_data_as_scatter(x_array, y_array):
-    plt.scatter(x_array, y_array)
+def run_KMean(data, k, random_state, type_of_data, algorithm='auto'):
+    kmeans = KMeans(n_clusters=k, random_state=random_state, algorithm=algorithm).fit_predict(data)
+    plt.title(f'{type_of_data} clustering, k = {k} ,algorithm = {algorithm}')
+    plt.scatter(data[:, 0], data[:, 1], c=kmeans)
+    plt.savefig(f'figures/{type_of_data}_cluster_k-{k}',bbox_inches='tight')
     plt.show()
+    return f'figures/{type_of_data}_cluster_k-{k}'
+
+
+def merge_x_y(x_data, y_data):
+    data = list()
+    for i in range(len(x_data)):
+        data.append([x_data[i], y_data[i]])
+    return np.array(data)
+
+
 
 
 if __name__ == '__main__':
-    # x, y = get_uniform_data(5)
+    # run_KMean(get_uniform_data(500, plot=False), 4, 170)
+    # run_KMean(get_gaussian_data([1, 2, 4], 0.5, 500, plot=False)[2], 4, 170)
+    # run_KMean(get_horizontal_clamps([(0,0) , (5,0), (0,2), (5,2)], 1, 0.25, 125, False), 5, 170)
+    # run_KMean(get_moon_b([-1, 1], [0, 2], False),  2, 170)
+    # run_KMean(get_moon_c([-1, 1], [0, 2], False),  2, 170)
+    run_KMean(get_letters_data(False), 3, 170, 'letters distribution')
+    # x, y = get_uniform_data(500, True)
     # plot_data(x, y)
     # get_gaussian_data([1, 2, 4], 0.5, 500)
     # get_horizontal_clamps([(0,0) , (5,0), (0,2), (5,2)], 1, 0.25, 125)
     # get_moon_b([-1, 1], [0, 2])
-    get_letters_data()
+    # get_letters_data()
